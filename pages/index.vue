338833dc282a128db716cfd6793ebcf754938ffa
matoi0317 <matoi.shibanuma@gmail.com>
2024-06-10 13:47:07 +0900

    a

--- pages/index.vue
+++ pages/index.vue
@@ -1,133 +1,143 @@
 <template>
   <div class="profile-container">
-    <header class="header">
+    <!-- <header class="header">
       <GlobalHeader />
-    </header>
-    <div class="main-content">
-      <div class="image-section">
-        <img src="../static/me2.JPG" alt="Profile Image" class="profile-image">
-        <div class="info-section">
-          <h1>Matoi Shibanuma</h1>
-          <p>(she/her)</p>
-          <p>Web Design</p>
+    </header> -->
+    <div class="header">
+        <div class="buttons">
+            <button @click="$router.push('/')" class="button is-primary is-outlined is-rounded">HOME</button>
+            <button @click="$router.push('/works')" class="button is-primary is-outlined is-rounded">WORKS</button>
+        </div>
+    </div>
+    <div class="main-container">
+      <div class="container-title">
+        <h1 class="name">MATOI SHIBANUMA</h1>
+        <div class="about">about</div>
+        <p>茨城県つくば市在住の女子大生(19)。小学生の頃からプログラミングに触れ現在はフロント周りを中心に制作。茨城県立並木中等教育学校卒業。津田塾大学総合政策学部総合政策学科所属。</p>
+        <div class="details">
+          <div class="container">
+            <h1 class="detail-title">Frontend</h1>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Vue.js</li>
+          </div>
+          <div class="container">
+            <h1 class="detail-title">Python</h1>
+            <li>Image Processing</li>
+            <li>Image Analysis</li>
+            <li>Voice Recognition</li>
+          </div>
         </div>
       </div>
-      <div class="about-section">
-        <h2>About Me</h2>
-        <hr />
-        <p>茨城県つくば市在住の女子大生です。茨城県立並木中等教育学校卒業、現在は津田塾大学総合政策学部総合政策学科に所属しています。Web制作、フロントエンド開発が好きです！使える言語はPython・Javascriptです。</p>
-        <div class="social-icons">
-          <i class="fab fa-instagram"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-facebook"></i>
-          <i class="fab fa-linkedin"></i>
-        </div>
-      </div>
+      <div class="container-image"></div>
     </div>
   </div>
 </template>
 
 <script>
-import GlobalHeader from "../components/GlobalHeader.vue";
+// import GlobalHeader from "../components/GlobalHeader.vue";
 
 export default {
-  components: {
-    GlobalHeader
-  }
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap');
 .profile-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-family: Arial, sans-serif;
+
+  .header {
+    width: 100%;
+    height: 20%;
+    display: flex;
+    justify-content: flex-end;  
+    background-color: #221F1F;
+    .buttons {
+        width: 50%;
+        margin: 20px;
+        display: flex;
+        justify-content: flex-end;
+        button {
+            // margin: 20px;
+            margin: 0 20px 0 0;
+        }
+    }
+  }
 }
 
 .header {
   width: 100%;
 }
 
-.main-content {
-  display: flex;
+.main-container {
   width: 100%;
-  margin-top: 50px;
-}
-
-.image-section {
+  height: 100vh;
+  background-color: #221F1F;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 50%;
-}
+  padding: 50px;
+  .container-image {
+    width: 30%;
+    height: 80%;
+    background-color: red;
+    border-radius: 50px;
+    background-image: url("../static/me2.JPG");
+    background-position: center;
+    background-size: cover;
+    margin-right: 40px;
+  }
+  .container-title {
+    width: 70%;
+    height: 80%;
+    .name {
+      font-weight: bold;
+      color: #A6BBCC;
+      font-size: 64px;
+      font-family: "Archivo Black", sans-serif;
+      margin: 0 0 0 40px;
+    }
+    p {
+      color: whitesmoke;
+      width: 80%;
+      text-indent: 1em;
+      margin: 0 0 20px 40px;
 
-.profile-image {
-  width: 80%;
-  /* border-radius: 50%; */
-}
-
-.info-section {
-  text-align: center;
-  margin-top: 20px;
-}
-
-.info-section h1 {
-  margin: 0;
-}
-
-.info-section p {
-  margin: 5px 0;
-}
-
-.email-button {
-  margin-top: 10px;
-  padding: 10px 20px;
-  background-color: black;
-  color: white;
-  border: none;
-  cursor: pointer;
-}
-
-.about-section {
-  background-color: #e0f0ff;
-  padding: 20px;
-  width: 50%;
-  margin-left: 20px;
-  margin-right: 80px;
-  border-radius: 10px;
-}
-
-.about-section h2 {
-  margin-top: 0;
-}
-
-.about-section hr {
-  margin: 10px 0;
-}
-
-.button-section {
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-}
-
-.resume-button, .portfolio-button {
-  padding: 10px 20px;
-  background-color: white;
-  border: 1px solid #000;
-  cursor: pointer;
-}
-
-.social-icons {
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-}
-
-.social-icons i {
-  font-size: 24px;
-  margin: 0 10px;
-  cursor: pointer;
+    }
+    .about {
+      width: 100%;
+      margin: 30px 40px 20px 40px;
+      font-size: 36px;
+      color: #A6BBCC;
+      font-family: "Archivo Black", sans-serif;
+      position: relative;
+      &::after {
+        content: "";
+        display: block;
+        width: 80%; /* 修正箇所 */
+        height: 2px;
+        background-color: #A6BBCC;
+        position: absolute;
+        bottom: 0; /* 修正箇所 */
+        left: 0;
+        transform: translateY(50%); /* 修正箇所 */
+      }
+    }
+    .details {
+      width: 100%;
+      margin: 0 40px;
+      display: flex;
+      .container {
+        width: 90%;
+        li {
+          padding: 10px 0;
+          color: #A6BBCC;
+        }
+        .detail-title {
+          color: #A6BBCC;
+          font-family: "Archivo Black", sans-serif;
+          font-size: 18px;
+        }
+      }
+    }
+  }
 }
 </style>
